64ac2f93db350c1c6796f5a80b88e37d01cf2125
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2022-03-03 14:38:11 +0530

    router basic

--- src/app/App.js
+++ src/app/App.js
@@ -7,6 +7,24 @@ import Checkout from './components/Checkout';
 
 import ThemeContext from './contexts/ThemeContext';
 
+import {BrowserRouter as Router, 
+        Route, 
+        Switch
+    } from 'react-router-dom';
+
+const Home = () => (<div>
+                        <h2>Home</h2>
+                    </div>)
+
+const About = () => (<div>
+    <h2>About</h2>
+</div>)
+
+
+const NotFound = () => (<div>
+    <h2>Page not Found!!</h2>
+</div>)
+
 // If we provide value for ThemeContext, then provided value shall be taken by consumer
 
 // class component, should have render function
@@ -25,25 +43,43 @@ class App extends React.Component {
 
         // return v.dom, write in jsx
         return (
-            <ThemeContext.Provider value={this.state.theme}>
-                <div>
-                    
-                    <button onClick={() => this.setState({theme: 'dark'})}>Dark</button>
-                    <button onClick={() => this.setState({theme: 'light'})}>Light</button>
-                    <button onClick={() => this.setState({theme: 'default'})}>Default</button>
+            <Router>
+                <ThemeContext.Provider value={this.state.theme}>
+                    <div>
+                        
+                        <button onClick={() => this.setState({theme: 'dark'})}>Dark</button>
+                        <button onClick={() => this.setState({theme: 'light'})}>Light</button>
+                        <button onClick={() => this.setState({theme: 'default'})}>Default</button>
 
-                    {/* app is parent component, Header, Footer are child components
-                        we pass data from app component to header, footer with properties
-                    */}
-                    <Header title="Product App" />
+                        {/* app is parent component, Header, Footer are child components
+                            we pass data from app component to header, footer with properties
+                        */}
+                        <Header title="Product App" />
+                            {/* Switch picks the first component that match router */}
+                        <Switch>
+                            <Route path="/cart">
+                                <Cart />
+                            </Route>
+                            <Route path="/checkout">
+                                <Checkout />
+                            </Route>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            {/* home path / will match all the routes */}
+                            <Route path="/" exact>
+                                <Home />
+                            </Route>
+                            {/* match all, but use for not found page, should be last one" */}
+                            <Route path="*">
+                                <NotFound />
+                            </Route>
+                        </Switch>
 
-                   <Checkout /> 
-         
-                    {/* <Cart /> */}
-
-                    <Footer year={2022} company="ShopMall" locations = { ['BLR', 'Chennai']} />
-                </div>
-            </ThemeContext.Provider>
+                        <Footer year={2022} company="ShopMall" locations = { ['BLR', 'Chennai']} />
+                    </div>
+                </ThemeContext.Provider>
+            </Router>
         )
     }
 }
